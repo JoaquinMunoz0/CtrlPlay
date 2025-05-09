d5c0c5a6ec04fd687c6c34ed88b94d1d21ca5124
JoaquinMunoz0 <145179105+JoaquinMunoz0@users.noreply.github.com>
2025-05-09 12:41:56 -0400

    Agregado Drawer
    Se agrego el drawer a la pantalla de inicio

---
+++ ctrplay/lib/pages/ctrlplay.dart
@@ -0,0 +1,104 @@
+import 'package:ctrplay/pages/splash.dart';
+import 'package:ctrplay/pages/profile.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Index 0: Inicio', style: optionStyle),
+    Text('Index 1: Mis Juegos', style: optionStyle),
+    Text('Index 2: Recomendados', style: optionStyle),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+      ),
+      body: Center(child: _widgetOptions[_selectedIndex]),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text('CtrlPlay'),
+            ),
+            ListTile(
+              title: const Text('Inicio'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(0);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Mis Juegos'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                // Update the state of the app
+                //_onItemTapped(1);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Profile()),
+                );
+                // Then close the drawer
+                //Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Recomendados'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                // Update the state of the app
+                //_onItemTapped(2);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Recomendados()),
+                );
+                // Then close the drawer
+                //Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
