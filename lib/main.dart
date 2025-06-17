6ac637e1589b6d4ecf133effb7935a8870d4c5b1
JoaquinMunoz0 <145179105+JoaquinMunoz0@users.noreply.github.com>
2025-06-16 20:12:49 -0400

    Mayor modularidad en el proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'splash.dart';
+import 'features/splash/screens/splash_screen.dart';
 
 void main() {
   runApp(const CtrlPlayApp());
